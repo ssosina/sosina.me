e9994a9f2d182583c98e706847aa0508e6d044eb
Samuel Sosina <samsosina@gmail.com>
2017-03-15 18:57:51 +0000

    Homepage edits
    - Added time checker
    - styled head and footer

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var greetingText;
+
+var currentDate = new Date();
+var currentHour = currentDate.getHours();
+
+if(currentHour < 12){
+  greetingText = "Good morning sunshine"
+}else if(currentHour < 18){
+ greetingText = "Good afternoon friend!"
+}else{
+	greetingText = "Good evening sleepy head"
+}
+
+jQuery("#greeting").text(greetingText);
