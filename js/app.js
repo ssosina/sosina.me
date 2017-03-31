b7d15be10e102bb6ac5a865c2f57c71e85a25e11
Samuel Sosina <samsosina@gmail.com>
2017-03-31 15:58:45 +0100

    Punctuation mistake

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ var currentDate = new Date();
 var currentHour = currentDate.getHours();
 
 if(currentHour < 12){
-  greetingText = "Good morning sunshine"
+  greetingText = "Good morning sunshine!"
 }else if(currentHour < 18){
  greetingText = "Good afternoon friend!"
 }else{
-	greetingText = "Good evening sleepy head"
+	greetingText = "Good evening sleepy head!"
 }
 
 jQuery("#greeting").text(greetingText);
