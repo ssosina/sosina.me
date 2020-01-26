a711ced72e71727bd3761384e2817deac40a30c3
Samuel Sosina <samsosina@gmail.com>
2020-01-26 15:07:26 +0000

    Portfolio 2020 update v1
    - Removed jekyll
    - Used CSS Grid and Flexbox for layout
    - Super simple layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,36 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
+/* Gulp is simply a build took */
 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+//Compile SCSS into CSS
+function style() {
+	// 1. Find SCSS file
+	return (
+		gulp
+			.src('./scss/**/*.scss')
+			// 2. Pass through sass compiler
+			.pipe(sass())
+			.on('error', sass.logError)
+			// 3. Where do save compiled css
+			.pipe(gulp.dest('./css'))
+			// 4. Stream changes to all browsers
+			.pipe(browserSync.stream())
+	);
+}
 
-/**
- * Build the Jekyll Site
- */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
+// Watch for changes and make updates
+function watch() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+	gulp.watch('./scss/**/*.scss', style);
+	gulp.watch('./*.html').on('change', browserSync.reload);
+	gulp.watch('./scripts/**/*.js').on('change', browserSync.reload);
+}
 
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
-});
-
-/**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
- */
-gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
-});
-
-/**
- * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
-gulp.task('watch', function () {
-    gulp.watch('_scss/**/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
-});
-
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
- */
-gulp.task('default', ['browser-sync', 'watch']);
+exports.style = style;
+exports.watch = watch;
